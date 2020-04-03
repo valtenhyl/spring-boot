b21c5c990102ecaccafb1833a8813e4cb80219eb
Valten123 <valtenhyl@163.com>
2020-04-03 12:35:23 +0800

    dynamic datasource

---
+++ spring-boot-dynamic/dynamic-datasource-aop/src/test/java/com/valten/DynamicApplicationTest.java
@@ -0,0 +1,30 @@
+package com.valten;
+
+import com.valten.service.DynamicDataSourceService;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * 多数据源测试
+ *
+ * @author Mark sunlightcs@gmail.com
+ */
+@SpringBootTest(classes = DynamicApplication.class)
+public class DynamicApplicationTest {
+
+    @Autowired
+    private DynamicDataSourceService dynamicDataSourceService;
+
+    @Test
+    public void test() {
+        Long id = 1L;
+
+        dynamicDataSourceService.updateUser(id);
+        dynamicDataSourceService.updateUserBySlave1(id);
+        dynamicDataSourceService.updateUserBySlave2(id);
+    }
+
+}
