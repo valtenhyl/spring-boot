e1e8e28cc4d17c5927cd39623388a5029a671dfb
Valten123 <valtenhyl@163.com>
2020-06-18 08:40:21 +0800

    springboot pageoffice

--- springboot-pageoffice/src/main/java/com/valten/controller/PageOfficeController.java
+++ springboot-pageoffice/src/main/java/com/valten/controller/PageOfficeController.java
@@ -3,11 +3,13 @@ package com.valten.controller;
 import com.zhuozhengsoft.pageoffice.FileSaver;
 import com.zhuozhengsoft.pageoffice.OpenModeType;
 import com.zhuozhengsoft.pageoffice.PageOfficeCtrl;
+import com.zhuozhengsoft.pageoffice.wordwriter.*;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -26,17 +28,61 @@ public class PageOfficeController {
         return mv;
     }
 
+    @RequestMapping(value = "/hello", method = RequestMethod.GET)
+    @ResponseBody
+    public String hello() {
+        return "hello world";
+    }
+
     @RequestMapping(value = "/word", method = RequestMethod.GET)
     public ModelAndView showWord(HttpServletRequest request) {
 
         PageOfficeCtrl poCtrl = new PageOfficeCtrl(request);
         // 设置服务页面
-        poCtrl.setServerPage("/poserver.zz");
+        poCtrl.setServerPage(request.getContextPath() + "/poserver.zz");
         // 添加自定义保存按钮
         poCtrl.addCustomToolButton("保存", "Save", 1);
         // 设置处理文件保存的请求方法
         poCtrl.setSaveFilePage("/save");
-        //打开word
+        WordDocument doc = new WordDocument();
+        //创建数据区域，createDataRegion 方法中的三个参数分别代表“新建的数据区域名称”，“数据区域将要插入的位置”，
+        //“与新建的数据区域相关联的数据区域名称”，若当前Word文档中尚无数据区域（书签）或者想在文档的最开头创建时，那么第三个参数为“[home]”
+        //若想在文档的结尾处创建数据区域则第三个参数为“[end]”
+        DataRegion dataRegion1 = doc.createDataRegion("reg1", DataRegionInsertType.After, "[home]");
+        //设置创建的数据区域的可编辑性
+        dataRegion1.setEditing(true);
+        //给数据区域赋值
+        dataRegion1.setValue("第一个数据区域\r\n");
+
+        DataRegion dataRegion2 = doc.createDataRegion("reg2", DataRegionInsertType.After, "reg1");
+        dataRegion2.setEditing(true);
+        dataRegion2.setValue("第二个数据区域");
+
+        DataRegion dataRegion3 = doc.createDataRegion("PO_table1", DataRegionInsertType.After, "reg2");
+        dataRegion3.setEditing(true);
+
+        //在word中指定的"PO_table1"的数据区域内动态创建一个3行5列的表格
+        Table table1 = doc.openDataRegion("PO_table1").createTable(3, 5, WdAutoFitBehavior.wdAutoFitWindow);
+        //合并(1,1)到(3,1)的单元格并赋值
+        table1.openCellRC(1, 1).mergeTo(3, 1);
+        table1.openCellRC(1, 1).setValue("合并后的单元格");
+        //给表格table1中剩余的单元格赋值
+        for (int i = 1; i < 4; i++) {
+            table1.openCellRC(i, 2).setValue("AA" + String.valueOf(i));
+            table1.openCellRC(i, 3).setValue("BB" + String.valueOf(i));
+            table1.openCellRC(i, 4).setValue("CC" + String.valueOf(i));
+            table1.openCellRC(i, 5).setValue("DD" + String.valueOf(i));
+        }
+
+        poCtrl.setWriter(doc);//此行必须
+
+        poCtrl.setWriter(doc);
+        //隐藏菜单栏
+        poCtrl.setMenubar(false);
+        //隐藏工具栏
+        poCtrl.setCustomToolbar(false);
+
+        //打开word文件
         poCtrl.webOpen("C:\\Users\\huang\\Desktop\\test.docx", OpenModeType.docAdmin, "张三");
 
         ModelAndView mv = new ModelAndView("Word");
